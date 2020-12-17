90b10c029d5d847a9749ce041330bd311dd4dfc4
Dmitry <dkillerevo@gmail.com>
2020-12-17 16:04:44 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int[] array = {1,2,8,4,9};
-        int cows = 3;
+        int angry_cows = 3;
         Alg algorithm = new Alg();
-        algorithm.search(array, cows);
+        algorithm.search(array, angry_cows);
     }
 }
